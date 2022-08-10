0a4f6a25bf5d30ef2db0bbfd3119f6d76fb46575
Naftali Kulik <nkulik1994@gmail.com>
2022-08-10 16:28:01 -0400

    configured action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "trivia_game_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    user_name: ENV['SMTP_USER_NAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
