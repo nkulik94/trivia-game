3f71519614ccfd93990c136aaa581f81b73f766d
Naftali Kulik <nkulik1994@gmail.com>
2022-08-10 16:49:08 -0400

    removed files for used for testing mailer

--- app/mailers/test_mailer.rb
+++
@@ -1,8 +0,0 @@
-class TestMailer < ApplicationMailer
-    default from: ENV['SMTP_USER_NAME']
-
-    def test_email
-        @user = params[:user]
-        mail(to: 'naftalikulikse@gmail.com', subject: "let's hope this works")
-    end
-end
