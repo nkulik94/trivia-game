c6b0f25c4846d55d9ba5c197ba9a76f2a5f9b022
Naftali Kulik <nkulik1994@gmail.com>
2022-08-11 13:12:28 -0400

    added code to generate questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,21 @@
 class Question < ApplicationRecord
+    validates :question, uniqueness: true
+
+    def self.get_and_save_questions token
+        questions_json = RestClient.get "https://opentdb.com/api.php?amount=50&type=multiple&token=#{token}"
+        questions = JSON.parse(questions_json)
+        questions['results'].each do |question|
+            new_question = create({
+                category: question['category'],
+                difficulty: question["difficulty"],
+                question: question['question'],
+                correct_answer: question['correct_answer'],
+                incorrect_answers: question['incorrect_answers'].join('|')
+            })
+            puts new_question.question
+            puts new_question.incorrect_answers
+        end
+        sleep(1)
+        get_and_save_questions(token) unless self.all.count > 1500 || questions['response_code'] != 0
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,10 @@ puts "getting token..."
 
 session = RestClient.get "https://opentdb.com/api_token.php?command=request"
 
-token = JSON.parse(session)[:token]
+token = JSON.parse(session)["token"]
 
-puts token
+puts "getting questions..."
+
+Question.get_and_save_questions(token)
+
+puts "done seeding!"
