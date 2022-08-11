658615c4bb1ce0c2a973fde00efb808b60f1393d
Naftali Kulik <nkulik1994@gmail.com>
2022-08-11 16:19:34 -0400

    added validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+    validates :username, presence: true, uniqueness: true
 end
