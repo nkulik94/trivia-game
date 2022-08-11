cd47aa694567f73859366da65ea79cfc4a2a6e82
Naftali Kulik <nkulik1994@gmail.com>
2022-08-11 13:47:21 -0400

    set up controller actions to handle user sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+
+    def show
+        if session[:user_id]
+            render json: User.find(session[:user_id]), status: :created
+        else
+            render json: { error: "Unauthorized" }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   resources :questions
+
+  post "/signup", to: "users#create"
+
+  get "/me", to: "users#show"
+
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
