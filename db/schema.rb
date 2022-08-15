5da4d4bb7848b1dd1a8078652e8d2af45922563f
Naftali Kulik <nkulik1994@gmail.com>
2022-08-15 16:30:45 -0400

    added avatar_url column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_11_172031) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_15_202908) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_172031) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url"
   end
 
 end
