64cae5911d83977cd9417a15dcaf03e91b1d1c62
Naftali Kulik <nkulik1994@gmail.com>
2022-08-15 12:19:58 -0400

    more testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,9 +34,9 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
-  #config.action_cable.mount_path = "/cable"
+  config.action_cable.mount_path = "/cable"
   config.action_cable.url = "wss://backward-jeopardy.herokuapp.com/cable"
-  #config.action_cable.allowed_request_origins = [ "http://backward-jeopardy.herokuapp.com", /http:\/\/backward-jeopardy.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ "http://backward-jeopardy.herokuapp.com", /http:\/\/backward-jeopardy.herokuapp.com.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
