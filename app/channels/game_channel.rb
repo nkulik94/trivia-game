ec122e3dee33d4bbac88f3b1a0326ad655219144
Naftali Kulik <nkulik1994@gmail.com>
2022-08-18 15:45:45 -0400

    set up class method for GameChannel to track subscribers

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,5 +1,12 @@
 class GameChannel < ApplicationCable::Channel
+  @@subscribers = {}
+
+  def self.subscribers
+    @@subscribers
+  end
+
   def subscribed
+    GameChannel.subscribers["#{current_user.id}"] = true
     stream_from "game_#{params[:game_id]}_channel"
   end
 
@@ -13,6 +20,14 @@ class GameChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    GameChannel.subscribers.delete "#{current_user.id}"
+    game = Game.find(params[:game_id])
+    if game.over?
+      stop_stream_from "game_#{params[:game_id]}_channel"
+    end
+    sleep(10)
+    if !GameChannel.subscribers["#{current_user.id}"] && !game.over?
+      ActionCable.server.broadcast("game_#{params[:game_id]}_channel", {forfeited: current_user.id})
+    end
   end
 end
--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,28 +1,62 @@
 import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
 import { GameContext } from '../context/game';
 import { UserContext } from '../context/user';
 import { CableContext } from '../context/cable';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
 
 function Game() {
     const gameContext = useContext(GameContext)
     const userContext = useContext(UserContext)
     const cableContext = useContext(CableContext)
+    const history = useHistory()
 
     const [channel, setChannel] = useState(null)
 
-    useEffect(() => {
+    function subscribeToGameChannel(id) {
         const gameChannel = cableContext.cable.subscriptions.create({
             channel: "GameChannel",
-            game_id: gameContext.game.id
+            game_id: id
         },
         {
-           received: console.log 
+           received: console.log
         })
 
         setChannel(gameChannel)
+    }
+
+    useEffect(() => {
+        if (gameContext.game) {
+            subscribeToGameChannel(gameContext.game.id)
+        } else {
+            fetch('/current-game')
+            .then(r => {
+                if (r.ok) {
+                    r.json().then(game => {
+                        gameContext.setGame(game)
+                        subscribeToGameChannel(game.id)
+                    })
+                } else {
+                    history.goBack()
+                }
+            })
+        }
     }, [])
 
-    return <button onClick={() => channel.send({difficulty: 'easy'})}>test</button>
+    //console.log(gameContext.game)
+
+    if (!gameContext.game) return <div></div>
+
+    return (
+        <Container sx={{width: '80%', height: 'fit-content', margin: 'auto', textAlign: 'center'}}>
+            <Paper>
+                <Typography variant="h2">{`${gameContext.game.player_1.name} vs ${gameContext.game.player_2.name}`}</Typography>
+            </Paper>
+        </Container>
+    )
 }
 
 export default Game
