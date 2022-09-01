0c67dd51088751a3960e34640d2615c5f849f0e6
Naftali Kulik <nkulik1994@gmail.com>
2022-09-01 13:55:17 -0400

    added validation to prevent user from setting stakes higher than available points

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,4 +1,9 @@
 class Challenge < ApplicationRecord
     validates :stakes, numericality: true
+    validates :stakes, comparison: { less_than_or_equal_to: :user_points }
     belongs_to :user
+
+    def user_points
+        self.user.points
+    end
 end
