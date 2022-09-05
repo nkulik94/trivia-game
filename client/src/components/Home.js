70c2c89e18f7197d211e60d10621b0f0de4eb88c
Naftali Kulik <nkulik1994@gmail.com>
2022-09-05 14:27:38 -0400

    created basic homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,37 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
+import { Link as RouterLink } from 'react-router-dom'
+import Container from "@mui/material/Container";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Link from '@mui/material/Link'
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
 function Home() {
-    const history = useHistory()
-    return <button onClick={() => history.push('/sign-in')}>login</button>
+    const user = useContext(UserContext).user
+
+    const loginBtns = (
+        <ButtonGroup>
+            <Button component={RouterLink} to='/sign-in'>log in</Button>
+            <Button component={RouterLink} to='/create-account'>create account</Button>
+        </ButtonGroup>
+    )
+
+    return (
+        <Container sx={{textAlign: 'center', marginTop: '2rem'}}>
+            <Paper sx={{padding: '2rem'}}>
+                <Typography variant="h2">Backward Jeopardy</Typography>
+                <Typography variant="h5">Where we ask the questions and you find the answers</Typography>
+                <br/>
+                <br/>
+                <Typography variant="body">Welcome to Backward Jeopardy! Here you can compete with other users for "points", which are kind meant to represent real money. Please be sure to <Link component={RouterLink} to='/rules'>read the rules</Link> before competing</Typography>
+                <br/>
+                <br/>
+                {user ? <Link component={RouterLink} to='/dashboard'>Dashboard</Link> : loginBtns}
+            </Paper>
+        </Container>
+    )
 }
 
 export default Home
