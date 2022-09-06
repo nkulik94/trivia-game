1c33998f38d2cedb49b0081388a52f4559b00502
Naftali Kulik <nkulik1994@gmail.com>
2022-09-06 13:38:07 -0400

    commented out code to get questions from open trivia db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "start seeding..."
 
-puts "getting token..."
+# puts "getting token..."
 
-session = RestClient.get "https://opentdb.com/api_token.php?command=request"
+# session = RestClient.get "https://opentdb.com/api_token.php?command=request"
 
-token = JSON.parse(session)["token"]
+# token = JSON.parse(session)["token"]
 
-puts "getting questions..."
+# puts "getting questions..."
 
-Question.get_all_questions(token)
+# Question.get_all_questions(token)
 
 puts "Creating users..."
 
